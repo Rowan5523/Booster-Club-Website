2f236df3365ae007dcc522c236dd67067d000c5a
Rowan5523 <124623286+Rowan5523@users.noreply.github.com>
2023-09-05 15:15:09 -0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,27 +1,32 @@
-// script1.js
-const carouselSlide = document.querySelector(".carousel-slide");
-const images = document.querySelectorAll(".carousel-slide img");
-const prevBtn = document.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
 
-let c = 0;
-const slideWidth = images[0].clientWidth * images.length; // Updated to calculate total width
+  const carouselSlide = document.querySelector(".carousel-slide");
+  const images = document.querySelectorAll(".carousel-slide img");
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
 
-function updateSlidePosition() {
-    carouselSlide.style.transform = `translateX(-${slideWidth * c}px)`; // Corrected translation
-}
+  let c = 0;
 
-function showNextSlide() {
+  // Calculate the total width of all images combined
+  const slideWidth = images[0].clientWidth * images.length;
+  
+  // Set the width of the carouselSlide element
+  carouselSlide.style.width = `${slideWidth}px`;
+
+  function updateSlidePosition() {
+    carouselSlide.style.transform = `translateX(-${images[0].clientWidth * c}px)`;
+  }
+
+  function showNextSlide() {
     c = (c + 1) % images.length;
     updateSlidePosition();
-}
+  }
 
-function showPrevSlide() {
+  function showPrevSlide() {
     c = (c - 1 + images.length) % images.length;
     updateSlidePosition();
-}
+  }
 
-nextBtn.addEventListener("click", showNextSlide);
-prevBtn.addEventListener("click", showPrevSlide);
+  nextBtn.addEventListener("click", showNextSlide);
+  prevBtn.addEventListener("click", showPrevSlide);
 
-updateSlidePosition();
+  updateSlidePosition();
