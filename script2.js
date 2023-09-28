ee37d7ce5cef7d6f8f9bd56936f513796be4281c
Rowan5523 <124623286+Rowan5523@users.noreply.github.com>
2023-09-27 20:59:02 -0400

    gimme penalty

--- script2.js
+++ script2.js
@@ -1,24 +1,45 @@
-// script2.js
 const image1 = document.getElementById('image4');
 const image2 = document.getElementById('image5');
 const image3 = document.getElementById('image9');
-
-image1.classList.add('active');
 
 const imageSources = ['sub.png', 'wrona.png', 'legion.png'];
 let currentIndex = 0;
 
 function changeImage() {
-    image1.classList.toggle('active');
-    image2.classList.toggle('active');
+    switch (currentIndex) {
+        case 0:
+            image1.classList.add('active');
+            image2.classList.remove('active');
+            image3.classList.remove('active');
+            break;
+        case 1:
+            image1.classList.remove('active');
+            image2.classList.add('active');
+            image3.classList.remove('active');
+            break;
+        case 2:
+            image1.classList.remove('active');
+            image2.classList.remove('active');
+            image3.classList.add('active');
+            break;
+    }
 
     currentIndex = (currentIndex + 1) % imageSources.length;
 
-    if (image1.classList.contains('active')) {
-        image2.src = imageSources[currentIndex];
+    if (currentIndex === 0) {
+        image1.src = imageSources[currentIndex];
+        image2.src = imageSources[currentIndex + 1];
+        image3.src = imageSources[imageSources.length - 1];
+    } else if (currentIndex === imageSources.length - 1) {
+        image1.src = imageSources[currentIndex];
+        image2.src = imageSources[0];
+        image3.src = imageSources[currentIndex - 1];
     } else {
         image1.src = imageSources[currentIndex];
+        image2.src = imageSources[currentIndex + 1];
+        image3.src = imageSources[currentIndex - 1];
     }
 }
 
 setInterval(changeImage, 5000);
+
