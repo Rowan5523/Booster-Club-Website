59179a52e5ba400735b2cc570a7b45b345396432
Rowan5523 <124623286+Rowan5523@users.noreply.github.com>
2025-02-09 16:52:39 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,8 +1,11 @@
 const image1 = document.getElementById('image4');
 const image2 = document.getElementById('image5');
 const image3 = document.getElementById('image9');
+const image4 = document.getElementById('image97');
+const image5 = document.getElementById('image98');
+const image6 = document.getElementById('image99');
 
-const imageSources = ['sub.png', 'wrona.png', 'legion.png'];
+const imageSources = ['sub.png', 'logo (2).jpeg', 'Woods RnR.jpeg', "Jay Kilmartin logo.png", "The melting pot screenshot.png", "Greater Chapin Community Foundation logo.png"];
 let currentIndex = 0;
 
 function changeImage() {
@@ -21,6 +24,15 @@ function changeImage() {
     } else if (currentIndex === 2) {
         image3.classList.add('active');
         image3.src = imageSources[currentIndex];
+    } else if (currentIndex === 3) {
+        image3.classList.add('active');
+        image3.src = imageSources[currentIndex];
+    } else if (currentIndex === 4) {
+        image3.classList.add('active');
+        image3.src = imageSources[currentIndex];
+    } else if (currentIndex === 5) {
+        image3.classList.add('active');
+        image3.src = imageSources[currentIndex];
     }
 }
 
@@ -29,4 +41,3 @@ changeImage();
 
 // Set an interval to change the image every 5 seconds
 setInterval(changeImage, 5000);
-
